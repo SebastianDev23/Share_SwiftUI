e8e44f823e30ae5b68176382f6becb84f1c0c835
Sebastian <s.kloesel@gmx.de>
2022-01-12 20:17:54 +0100

    Initial Commit

---
+++ Share_SwiftUI/Share_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Share_SwiftUIApp.swift
+//  Share_SwiftUI
+//
+//  Created by Sebastian Klösel on 12.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Share_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
